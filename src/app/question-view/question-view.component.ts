4fb1b1c4bf80e540f93fabb831ad56a58eaaf233
bryant <bryantphamm@gmail.com>
2016-05-23 21:54:04 -0500

    Implemented prev and next question toggling

--- src/app/question-view/question-view.component.ts
+++ src/app/question-view/question-view.component.ts
@@ -13,17 +13,36 @@ export class QuestionViewComponent {
   questions: Array<Question>;
   assignment: string;
   currentQuestion: Question;
+  currentQuestionIndex: number = 0;
 
   ngOnInit() {
     this.assignment = 'Social Studies DA';
     this.questions = [
       new Question(1, 4, 'A'),
-      new Question(1, 4, 'A'),
-      new Question(1, 4, 'A'),
-      new Question(1, 4, 'A'),
-      new Question(1, 4, 'A'),
-      new Question(1, 4, 'A')
+      new Question(2, 4, 'A'),
+      new Question(3, 4, 'A'),
+      new Question(4, 4, 'A'),
+      new Question(5, 4, 'A'),
+      new Question(6, 4, 'A')
     ];
-    this.currentQuestion = this.questions[0];
+    this.setCurrentQuestion();
+  }
+
+  prevQuestion(): void {
+    if (this.currentQuestionIndex !== 0) {
+      this.currentQuestionIndex--;
+      this.setCurrentQuestion();
+    }
+  }
+
+  nextQuestion(): void {
+    if (this.currentQuestionIndex !== this.questions.length-1) {
+      this.currentQuestionIndex++;
+      this.setCurrentQuestion();
+    }
+  }
+
+  setCurrentQuestion(): void {
+    this.currentQuestion = this.questions[this.currentQuestionIndex];
   }
 }
