891b31d9e060ceaeb05d4c55e884391fe1677f95
bryant <bryantphamm@gmail.com>
2016-05-29 09:56:00 -0500

    feature(assignment-view): Add Assignment model

---
+++ src/app/shared/models/assignment.model.ts
@@ -0,0 +1,34 @@
+import { Question } from "./question.model";
+
+export class Assignment {
+  name: string;
+  questions: Array<Question>;
+  dueDate: Date;
+  numOfQuestions; number;
+
+  constructor(name: string, questions: Array<Question>, dueDate: Date) {
+    this.name = name;
+    this.questions = questions;
+    this.dueDate = dueDate;
+    this.numOfQuestions = this.questions.length;
+  }
+}
+
+export module Assignment {
+  export class TestBuilder {
+    static buildAssignments(...assignmentNames: Array<string>): Array<Assignment> {
+      let assignments: Array<Assignment> = [];
+      for (let name of assignmentNames) {
+        assignments.push(TestBuilder.buildAssignment(name));
+      }
+      return assignments;
+    }
+    
+    static buildAssignment(assignmentName: string): Assignment {
+      return new Assignment(
+        assignmentName,
+        [new Question('1', 4, 'A')],
+        new Date());
+    }
+  }
+}
