1048a6d0ba3ffb6c86ddc2965cac890fe22e47c9
bryant <bryantphamm@gmail.com>
2016-05-31 23:50:37 -0500

    Refactor home component to use home-config and implemented navigation to tile routes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router-deprecated';
+
+import { HomeConfig } from "./models/home-config";
+import { TEACHER_HOME_CONFIG } from "./models/teacher-home-config";
 
 @Component({
   selector: 'bro-student-home',
@@ -6,5 +10,13 @@ import { Component } from '@angular/core';
   styles: [ require('./home.css') ]
 })
 export class HomeComponent {
-  
+  CONFIG: HomeConfig;
+
+  constructor(private router: Router) {
+    this.CONFIG = TEACHER_HOME_CONFIG;
+  }
+
+  navigateTo(routerKey: string) {
+    this.router.navigate([routerKey]);
+  }
 }
