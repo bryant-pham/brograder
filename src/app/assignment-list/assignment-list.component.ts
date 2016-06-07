cae11d6a9493647002a9eb121e263299892da66a
bryant <bryantphamm@gmail.com>
2016-06-07 01:32:15 -0500

    Put assignment search box and pipe into assignment list component

--- src/app/assignment-list/assignment-list.component.ts
+++ src/app/assignment-list/assignment-list.component.ts
@@ -2,15 +2,20 @@ import { Component } from '@angular/core';
 
 import { Assignment } from '../shared/models/assignment.model';
 import { AssignmentService } from '../shared/services/assignment.service';
+import { AssignmentSearch } from './assignment-search.component';
+import { AssignmentSearchPipe } from "../shared/pipes/assignment-search.pipe";
 
 @Component({
   selector: 'bro-assignment-list',
   template: require('./assignment-list.html'),
   styles: [ require('./assignment-list.css') ],
-  providers: [ AssignmentService ]
+  providers: [ AssignmentService ],
+  directives: [ AssignmentSearch ],
+  pipes: [ AssignmentSearchPipe ]
 })
 export class AssignmentListComponent {
   assignments: Array<Assignment> = [];
+  searchInput: string;
 
   constructor(private assignmentService: AssignmentService) {
     this.assignmentService.getAllAssignments()
@@ -19,5 +24,9 @@ export class AssignmentListComponent {
 
   setCurrentAssignment(assignment: Assignment): void {
     this.assignmentService.setCurrentAssignment(assignment);
+  }
+
+  handleSearchInput(event): void {
+    this.searchInput = event.value;
   }
 }
