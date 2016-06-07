70d82fd91822754504868bd3ea8a73ec04c0579e
bryant <bryantphamm@gmail.com>
2016-06-06 21:48:45 -0500

    Refactor class selector component to use dropdown menu component

--- src/app/class/class-selector.component.ts
+++ src/app/class/class-selector.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 
 import { Class } from "../shared/models";
 import { ClassService } from "../shared/services/class.service.ts";
+import { DropdownMenuComponent } from "../shared/dropdown-menu/dropdown-menu.component";
 
 @Component({
   selector: 'bro-class-selector',
   template: require('./class-selector.html'),
-  providers: [ ClassService ]
+  providers: [ ClassService ],
+  directives: [ DropdownMenuComponent ]
 })
 export class ClassSelectorComponent {
   classes: Array<Class>;
@@ -14,5 +16,9 @@ export class ClassSelectorComponent {
   constructor(private classService: ClassService) {
     this.classService.getAllClasses()
       .subscribe(classes => this.classes = classes);
+  }
+
+  handleClassSelect(event): void {
+    this.classService.setCurrentClass(event.value);
   }
 }
