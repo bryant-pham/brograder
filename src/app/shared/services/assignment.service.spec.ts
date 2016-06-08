74d1dfc2180ebfd0fef061850c7b0bebd88684e4
bryant <bryantphamm@gmail.com>
2016-06-07 21:53:16 -0500

    Added assignment service tests

--- src/app/shared/services/assignment.service.spec.ts
+++ src/app/shared/services/assignment.service.spec.ts
@@ -6,30 +6,60 @@ import {
   beforeEach
 } from '@angular/core/testing';
 import { provideStore } from '@ngrx/store';
+import { Store } from '@ngrx/store';
 
 import { AssignmentService } from './assignment.service';
-import { CURRENT_ASSIGNMENT_REDUCER } from '../reducers/current-assignment.reducer';
+import { AppStore } from '../reducers/app.store';
 import { Assignment } from '../models/assignment.model';
+import {
+  REDUCERS,
+  CURRENT_ASSIGNMENT,
+  SET_CURRENT_ASSIGNMENT,
+  SET_ASSIGNMENTS
+} from '../reducers/index';
 
 describe('AssignmentService', () => {
   beforeEachProviders(() => [
     AssignmentService,
-    provideStore(CURRENT_ASSIGNMENT_REDUCER)
+    provideStore(REDUCERS)
   ]);
 
   let service: AssignmentService;
+  let store: Store<AppStore>;
 
-  beforeEach(inject([AssignmentService], (serv) => {
+  beforeEach(inject([AssignmentService, Store], (serv, sto) => {
     service = serv;
+    store = sto;
   }));
 
-  it('should set and get current assignment', () => {
+  it('should set current assignment', () => {
     let expectedAssignment = Assignment.TestBuilder.buildAssignment('test');
+
     service.setCurrentAssignment(expectedAssignment);
 
+    store.select(CURRENT_ASSIGNMENT)
+      .subscribe(currentAssignment => {
+        expect(currentAssignment).toBe(expectedAssignment);
+      });
+  });
+
+  it('should get current assignment', () => {
+    let expectedAssignment = Assignment.TestBuilder.buildAssignment('test');
+    store.dispatch({type: SET_CURRENT_ASSIGNMENT, payload: expectedAssignment});
+
     service.getCurrentAssignment()
-      .subscribe((assignment) => {
-        expect(assignment).toEqual(expectedAssignment);
-    });
+      .subscribe(currentAssignment => {
+        expect(currentAssignment).toBe(expectedAssignment);
+      });
+  });
+
+  it('should get all assignments', () => {
+    let expectedAssignments = [Assignment.TestBuilder.buildAssignment('test')];
+    store.dispatch({type: SET_ASSIGNMENTS, payload: expectedAssignments});
+
+    service.getAllAssignments()
+      .subscribe(assignments => {
+        expect(assignments).toBe(expectedAssignments);
+      });
   });
 });
