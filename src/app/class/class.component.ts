31b65351fd34bf6c3d08ce2d951d323d9061afe4
bryant <bryantphamm@gmail.com>
2016-06-09 01:12:21 -0500

    Refactor class component to be a reusable instead of a full page view

--- src/app/class/class.component.ts
+++ src/app/class/class.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 import { Class } from '../shared/models';
 import { ClassService } from '../shared/services/class.service.ts';
@@ -7,10 +7,12 @@ import { ClassSelectorComponent } from './class-selector.component';
 @Component({
   selector: 'bro-class',
   template: require('./class.html'),
+  styles: [ require('../shared/styles/shared.css') ],
   providers: [ ClassService ],
   directives: [ ClassSelectorComponent ]
 })
 export class ClassComponent {
+  @Input() route: string;
   currentClass: Class;
 
   constructor(private classService: ClassService) {
