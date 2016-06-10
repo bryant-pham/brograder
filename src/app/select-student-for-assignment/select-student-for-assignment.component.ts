a2e86e06d09947bab6c0134b64a7f943451d6f6e
bryant <bryantphamm@gmail.com>
2016-06-09 21:21:36 -0500

    Add current assignment text in select student for assignment component

--- src/app/select-student-for-assignment/select-student-for-assignment.component.ts
+++ src/app/select-student-for-assignment/select-student-for-assignment.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { ClassComponent } from '../class/class.component';
+import { AssignmentService } from '../shared/services';
+import { Assignment } from '../shared/models';
 
 @Component({
   selector: 'bro-pick-student-for-assignment',
   template: require('./select-student-for-assignment.html'),
+  providers: [ AssignmentService ],
   directives: [ ClassComponent ]
 })
-export class SelectForAssignmentComponent {
+export class SelectForAssignmentComponent implements OnInit {
   nextRoute = 'Questions';
+  assignment: Assignment;
+
+  constructor(private assignmentService: AssignmentService) {
+  }
+
+  ngOnInit() {
+    this.assignmentService.getCurrentAssignment()
+      .subscribe(assignment => this.assignment = assignment);
+  }
 }
