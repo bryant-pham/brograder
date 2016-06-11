12f638a0eb1fcdf6276488248b3d4678da283138
bryant <bryantphamm@gmail.com>
2016-06-11 00:58:32 -0500

    Add tests for changePossibleAnswers in add-assignment component

--- src/app/add-assignment/add-assignment.component.spec.ts
+++ src/app/add-assignment/add-assignment.component.spec.ts
@@ -29,4 +29,52 @@ describe('AddAssignmentComponent', () => {
 
     component.closeModal();
   });
+
+  it('there should be no possible answers if numOfAnswersPerQuestion is 0', () => {
+    component.numAnswersPerQuestion = 0;
+
+    component.changePossibleAnswers();
+
+    expect(component.possibleAnswers).toEqual([]);
+  });
+
+  it('possible answers should be A if numOfAnswersPerQuestion is 1', () => {
+    component.numAnswersPerQuestion = 1;
+
+    component.changePossibleAnswers();
+
+    expect(component.possibleAnswers).toEqual(['A']);
+  });
+
+  it('possible answers should be A, B if numOfAnswersPerQuestion is 2', () => {
+    component.numAnswersPerQuestion = 2;
+
+    component.changePossibleAnswers();
+
+    expect(component.possibleAnswers).toEqual(['A', 'B']);
+  });
+
+  it('possible answers should be A, B, C if numOfAnswersPerQuestion is 3', () => {
+    component.numAnswersPerQuestion = 3;
+
+    component.changePossibleAnswers();
+
+    expect(component.possibleAnswers).toEqual(['A', 'B', 'C']);
+  });
+
+  it('possible answers should be A, B, C, D if numOfAnswersPerQuestion is 4', () => {
+    component.numAnswersPerQuestion = 4;
+
+    component.changePossibleAnswers();
+
+    expect(component.possibleAnswers).toEqual(['A', 'B', 'C', 'D']);
+  });
+
+  it('possible answers should be A, B, C, D, E if numOfAnswersPerQuestion is 5', () => {
+    component.numAnswersPerQuestion = 5;
+
+    component.changePossibleAnswers();
+
+    expect(component.possibleAnswers).toEqual(['A', 'B', 'C', 'D', 'E']);
+  });
 });
