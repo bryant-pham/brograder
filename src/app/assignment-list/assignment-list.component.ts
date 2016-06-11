b51bef86abcfddcb6e5d2cb197af256598df946a
bryant <bryantphamm@gmail.com>
2016-06-10 22:40:29 -0500

    Add add-assignment modal to assignment-list component

--- src/app/assignment-list/assignment-list.component.ts
+++ src/app/assignment-list/assignment-list.component.ts
@@ -3,17 +3,20 @@ import { Component } from '@angular/core';
 import { Assignment } from '../shared/models/assignment.model';
 import { AssignmentService } from '../shared/services/assignment.service';
 import { AssignmentSearchPipe } from '../shared/pipes/assignment-search.pipe';
+import { AddAssignmentComponent } from '../add-assignment/add-assignment.component';
 
 @Component({
   selector: 'bro-assignment-list',
   template: require('./assignment-list.html'),
   styles: [ require('./assignment-list.css') ],
   providers: [ AssignmentService ],
-  pipes: [ AssignmentSearchPipe ]
+  pipes: [ AssignmentSearchPipe ],
+  directives: [ AddAssignmentComponent ]
 })
 export class AssignmentListComponent {
   assignments: Array<Assignment> = [];
   searchTerm: string;
+  showAddAssignmentModal = false;
 
   constructor(private assignmentService: AssignmentService) {
     this.assignmentService.getAllAssignments()
@@ -22,5 +25,13 @@ export class AssignmentListComponent {
 
   setCurrentAssignment(assignment: Assignment): void {
     this.assignmentService.setCurrentAssignment(assignment);
+  }
+
+  closeModal(): void {
+    this.showAddAssignmentModal = false;
+  }
+
+  showModal(): void {
+    this.showAddAssignmentModal = true;
   }
 }
