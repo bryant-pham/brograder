41cfad19ebe090418c7f9a6652f9858188f94928
bryant <bryantphamm@gmail.com>
2016-06-14 18:32:31 -0500

    Added addGradedAssignment method to student model

---
+++ src/app/shared/models/student.model.spec.ts
@@ -0,0 +1,16 @@
+import { describe, it, expect } from '@angular/core/testing';
+
+import { Student } from './student.model';
+import { Assignment } from './assignment.model';
+import { GradedAssignment } from './gradedAssignment.model';
+
+describe('Student', () => {
+  it('should add graded assignment', () => {
+    let student = new Student();
+    let gradedAssignment = new GradedAssignment(Assignment.TestBuilder.buildAssignment('test'));
+
+    student.addGradedAssignment(gradedAssignment);
+
+    expect(student.gradedAssignments).toEqual([gradedAssignment]);
+  });
+});
--- src/app/shared/models/student.model.ts
+++ src/app/shared/models/student.model.ts
@@ -1,9 +1,17 @@
+import { GradedAssignment } from './gradedAssignment.model';
+
 export class Student {
   firstName: string;
   lastName: string;
+  gradedAssignments: Array<GradedAssignment>;
 
   constructor(firstName: string, lastName: string) {
     this.firstName = firstName;
     this.lastName = lastName;
+    this.gradedAssignments = [];
+  }
+
+  addGradedAssignment(gradedAssignment: GradedAssignment) {
+    this.gradedAssignments.push(gradedAssignment);
   }
 }
