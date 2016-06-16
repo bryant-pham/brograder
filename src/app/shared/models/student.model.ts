6c69e61ad27c2c86100064bbe8eee5f066e662ea
bryant <bryantphamm@gmail.com>
2016-06-15 23:04:44 -0500

    Add id attribute to student model

--- src/app/shared/models/student.model.ts
+++ src/app/shared/models/student.model.ts
@@ -1,14 +1,16 @@
 import { GradedAssignment } from './gradedAssignment.model';
 
 export class Student {
+  id: string;
   firstName: string;
   lastName: string;
   gradedAssignments: Map<string, GradedAssignment>;
 
-  constructor(firstName: string, lastName: string) {
+  constructor(firstName: string, lastName: string, id?: string) {
     this.firstName = firstName;
     this.lastName = lastName;
     this.gradedAssignments = new Map<string, GradedAssignment>();
+    this.id = id;
   }
 
   addGradedAssignment(gradedAssignment: GradedAssignment) {
