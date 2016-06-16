489b4647d4914e370b3ada47b53ade5c38bd9c8d
bryant <bryantphamm@gmail.com>
2016-06-15 22:10:46 -0500

    Add rxjs first operator import

--- src/vendor.ts
+++ src/vendor.ts
@@ -14,6 +14,7 @@ import '@angular/router-deprecated';
 // RxJS
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/mergeMap';
+import 'rxjs/add/operator/first';
 
 // Angular 2 Material 2
 import '@angular2-material/button';
