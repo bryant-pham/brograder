3146c7a8732166f3b873f46a91482e3516cb67e8
bryant <bryantphamm@gmail.com>
2016-06-18 16:59:57 -0500

    Created students reducer

--- src/app/shared/reducers/index.ts
+++ src/app/shared/reducers/index.ts
@@ -3,17 +3,20 @@ import { currentClassReducer } from './current-class.reducer';
 import { classesReducer } from './classes.reducer.ts';
 import { assignmentsReducer } from './assignments.reducer';
 import { currentStudentReducer } from './current-student.reducer';
+import { studentsReducer } from './students.reducer';
 
 export * from './current-assignment.reducer';
 export * from './current-class.reducer';
 export * from './classes.reducer.ts';
 export * from './assignments.reducer';
 export * from './current-student.reducer';
+export * from './students.reducer';
 
 export const REDUCERS = {
   CURRENT_ASSIGNMENT: currentAssignmentReducer,
   CURRENT_CLASS: currentClassReducer,
   CLASSES: classesReducer,
   ASSIGNMENTS: assignmentsReducer,
-  CURRENT_STUDENT: currentStudentReducer
+  CURRENT_STUDENT: currentStudentReducer,
+  STUDENTS: studentsReducer
 };
