3957a2f2628abf50e64bcc9b71ce8f72f71e89ee
bryant <bryantphamm@gmail.com>
2016-06-18 16:07:35 -0500

    Add teacherId to student model

--- src/app/shared/models/student.model.ts
+++ src/app/shared/models/student.model.ts
@@ -2,15 +2,17 @@ import { GradedAssignment } from './gradedAssignment.model';
 
 export class Student {
   id: string;
+  teacherId: string;
   firstName: string;
   lastName: string;
   gradedAssignments: Map<string, GradedAssignment>;
 
-  constructor(firstName: string, lastName: string, id?: string) {
+  constructor(firstName: string, lastName: string, id?: string, teacherId?: string) {
     this.firstName = firstName;
     this.lastName = lastName;
     this.gradedAssignments = new Map<string, GradedAssignment>();
     this.id = id;
+    this.teacherId = teacherId;
   }
 
   addGradedAssignment(gradedAssignment: GradedAssignment) {
