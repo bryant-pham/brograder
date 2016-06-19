6e3cc464f0db3dd1d53aa06146d5a5eabb35b401
bryant <bryantphamm@gmail.com>
2016-06-19 16:45:15 -0500

    Refactor class-selector component to emit selected class as event

--- src/app/student-list/class-selector.component.ts
+++ src/app/student-list/class-selector.component.ts
@@ -1,24 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
-import { Class } from '../shared/models';
-import { ClassService } from '../shared/services/class.service.ts';
+import { TeacherService } from '../shared/services';
 import { DropdownMenuComponent } from '../shared/dropdown-menu/dropdown-menu.component';
 
 @Component({
   selector: 'bro-class-selector',
   template: require('./class-selector.html'),
-  providers: [ ClassService ],
+  providers: [ TeacherService ],
   directives: [ DropdownMenuComponent ]
 })
 export class ClassSelectorComponent {
-  classes: Array<Class>;
+  classes: Array<string>;
+  @Output() selected = new EventEmitter();
 
-  constructor(private classService: ClassService) {
-    this.classService.getAllClasses()
-      .subscribe(classes => this.classes = classes);
+  constructor(private teacherService: TeacherService) {
+    this.teacherService.getTeacher()
+      .subscribe(teacher => this.classes = teacher.classes);
   }
 
-  handleClassSelect(event): void {
-    this.classService.setCurrentClass(event.value);
+  handleClassSelect(selectionEvent): void {
+    this.selected.emit(selectionEvent);
   }
 }
