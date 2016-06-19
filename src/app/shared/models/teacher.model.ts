d09db942d79253847f9735d55651ab3a2157fea9
bryant <bryantphamm@gmail.com>
2016-06-19 10:56:00 -0500

    Add classes attribute to teacher model

--- src/app/shared/models/teacher.model.ts
+++ src/app/shared/models/teacher.model.ts
@@ -1,22 +1,30 @@
 export class Teacher {
   id: string;
+  classes: Array<string>;
 
-  constructor(id: string) {
+  constructor(id: string, classes: Array<string>) {
     this.id = id;
+    this.classes = classes;
   }
 }
 
 export module Teacher {
   export class Builder {
     id = '1';
+    classes = [];
 
     withId(id: string): Builder {
       this.id = id;
       return this;
     }
 
+    withClasses(classes: Array<string>) {
+      this.classes = classes;
+      return this;
+    }
+
     build(): Teacher {
-      return new Teacher(this.id);
+      return new Teacher(this.id, this.classes);
     }
   }
 }
