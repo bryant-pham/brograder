44974e0443b729c92177a065f7be8c799d9725b1
bryant <bryantphamm@gmail.com>
2016-06-19 11:21:10 -0500

    Refactor student reducer initial state to use builder

--- src/app/shared/reducers/students.reducer.ts
+++ src/app/shared/reducers/students.reducer.ts
@@ -7,13 +7,23 @@ export const STUDENTS = 'STUDENTS';
 
 export const SET_STUDENTS = 'SET_STUDENTS';
 
+let john = new Student.Builder()
+  .withId('1')
+  .withFirstName('John')
+  .withFirstName('Cena')
+  .withClass('AM')
+  .withTeacherId('1')
+  .build();
+let andy = new Student.Builder()
+  .withId('2')
+  .withFirstName('Andy')
+  .withFirstName('Murray')
+  .withClass('PM')
+  .withTeacherId('1')
+  .build();
 let initialState = Immutable.Map<string, Student>()
-  .set('1', new Student('John', 'Cena', '1'))
-  .set('2', new Student('Novak', 'Djokovic', '2'))
-  .set('3', new Student('Andy', 'Murray', '3'))
-  .set('4', new Student('Jack', 'Cena', '4'))
-  .set('5', new Student('Mike', 'Djokovic', '5'))
-  .set('6', new Student('Roger', 'Gracie', '6'));
+  .set('1', john)
+  .set('2', andy);
 
 export const studentsReducer: ActionReducer<Immutable.Map<string, Student>> = (state = initialState, {type, payload}) => {
   switch (type) {
