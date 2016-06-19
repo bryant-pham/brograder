41f46c55bca37a33af72c964b65e9ac2ac562b8c
bryant <bryantphamm@gmail.com>
2016-06-19 12:45:04 -0500

    Refactor dropdown component to work on non primitive types

--- src/app/shared/dropdown-menu/dropdown-menu.component.ts
+++ src/app/shared/dropdown-menu/dropdown-menu.component.ts
@@ -8,9 +8,17 @@ export class DropdownMenuComponent {
   @Input() inputData: Array<any>;
   @Input() displayAttribute: string;
   @Input() label: string;
+  @Input() isObject: boolean = false;
   @Output() selected = new EventEmitter();
 
   selectedValue: any;
+
+  getDisplayValue(data): string {
+    if (this.isObject) {
+      return data[this.displayAttribute];
+    }
+    return data;
+  }
 
   handleSelection() {
     this.selected.emit({value: this.selectedValue});
