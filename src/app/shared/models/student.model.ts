5274bddfef5aac8c0484a8be4c4423488ec0278c
bryant <bryantphamm@gmail.com>
2016-06-19 11:15:14 -0500

    Add className to student model and add builder

--- src/app/shared/models/student.model.ts
+++ src/app/shared/models/student.model.ts
@@ -6,16 +6,74 @@ export class Student {
   firstName: string;
   lastName: string;
   gradedAssignments: Map<string, GradedAssignment>;
+  className: string;
 
-  constructor(firstName: string, lastName: string, id?: string, teacherId?: string) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.gradedAssignments = new Map<string, GradedAssignment>();
+  constructor(id: string,
+              teacherId: string,
+              firstName: string,
+              lastName: string,
+              className: string,
+              gradedAssignments?: Map<string, GradedAssignment>) {
     this.id = id;
     this.teacherId = teacherId;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.className = className;
+
+    if (gradedAssignments) {
+      this.gradedAssignments = gradedAssignments;
+    } else {
+      this.gradedAssignments = new Map<string, GradedAssignment>();
+    }
   }
 
   addGradedAssignment(gradedAssignment: GradedAssignment) {
     this.gradedAssignments.set(gradedAssignment.getId(), gradedAssignment);
+  }
+}
+
+export module Student {
+  export class Builder {
+    id = '1';
+    teacherId = '1';
+    firstName = 'john';
+    lastName = 'cena';
+    gradedAssignments = new Map<string, GradedAssignment>();
+    className: string;
+
+    withId(id: string): Builder {
+      this.id = id;
+      return this;
+    }
+
+    withTeacherId(id: string): Builder {
+      this.teacherId = id;
+      return this;
+    }
+
+    withFirstName(name: string): Builder {
+      this.firstName = name;
+      return this;
+    }
+
+    withLastName(name: string): Builder {
+      this.lastName = name;
+      return this;
+    }
+
+    withClass(className: string): Builder {
+      this.className = className;
+      return this;
+    }
+
+    build(): Student {
+      return new Student(
+        this.id,
+        this.teacherId,
+        this.firstName,
+        this.lastName,
+        this.gradedAssignments,
+        this.className);
+    }
   }
 }
