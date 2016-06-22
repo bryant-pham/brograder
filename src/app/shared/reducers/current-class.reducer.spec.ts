22cd504b83dc9d663d0e3859e259f742ed01f6f0
bryant <bryantphamm@gmail.com>
2016-06-22 01:46:16 -0500

    Refactor current class reducer to be of string type

--- src/app/shared/reducers/current-class.reducer.spec.ts
+++ src/app/shared/reducers/current-class.reducer.spec.ts
@@ -1,11 +1,10 @@
 import { describe, it, expect } from '@angular/core/testing';
 
-import { Class } from '../models';
 import { SET_CURRENT_CLASS, currentClassReducer } from './current-class.reducer';
 
 describe('current-class reducer', () => {
   it('set current class', () => {
-    let payload = Class.Builder.build('test');
+    let payload = 'AM';
 
     let state = currentClassReducer(undefined, {type: SET_CURRENT_CLASS, payload: payload});
 
@@ -13,8 +12,8 @@ describe('current-class reducer', () => {
   });
 
   it('perform no operation if invalid reducer operation', () => {
-    let initialState = Class.Builder.build('initial');
-    let payload = Class.Builder.build('test');
+    let initialState = 'AM';
+    let payload = 'PM';
 
     let state = currentClassReducer(
       initialState, {type: 'INVALID REDUCER OPERATION', payload: payload});
--- src/app/shared/reducers/current-class.reducer.ts
+++ src/app/shared/reducers/current-class.reducer.ts
@@ -1,12 +1,10 @@
 import { ActionReducer } from '@ngrx/store';
 
-import { Class } from '../models';
-
 export const CURRENT_CLASS = 'CURRENT_CLASS';
 
 export const SET_CURRENT_CLASS = 'SET_CURRENT_CLASS';
 
-export const currentClassReducer: ActionReducer<Class> = (state, {type, payload}) => {
+export const currentClassReducer: ActionReducer<string> = (state, {type, payload}) => {
   switch (type) {
     case SET_CURRENT_CLASS:
       return payload;
