19a9e710b8ede995df576720896cf713a8fdeb96
bryant <bryantphamm@gmail.com>
2016-06-22 01:47:00 -0500

    Refactor class service to get and set current class as string

--- src/app/shared/services/class.service.spec.ts
+++ src/app/shared/services/class.service.spec.ts
@@ -9,12 +9,10 @@ import { provideStore, Store } from '@ngrx/store';
 
 import { ClassService } from './class.service';
 import { AppStore } from '../reducers/app.store';
-import { Class } from '../models';
 import {
   REDUCERS,
   SET_CURRENT_CLASS,
   CURRENT_CLASS,
-  SET_ALL_CLASSES
 } from '../reducers/index';
 
 describe('ClassService', () => {
@@ -32,7 +30,7 @@ describe('ClassService', () => {
   }));
 
   it('should get current class', () => {
-    let expectedClass = Class.Builder.build('class');
+    let expectedClass = 'AM';
     store.dispatch({type: SET_CURRENT_CLASS, payload: expectedClass});
 
     service.getCurrentClass()
@@ -42,23 +40,13 @@ describe('ClassService', () => {
   });
 
   it('should set current class', () => {
-    let expectedClass = Class.Builder.build('class');
+    let expectedClass = 'AM';
 
     service.setCurrentClass(expectedClass);
 
     store.select(CURRENT_CLASS)
       .subscribe(currentClass => {
         expect(currentClass).toEqual(expectedClass);
-      });
-  });
-
-  it('should get all classes', () => {
-    let expectedClasses = [Class.Builder.build('class')];
-    store.dispatch({type: SET_ALL_CLASSES, payload: expectedClasses});
-
-    service.getAllClasses()
-      .subscribe(classes => {
-        expect(classes).toEqual(expectedClasses);
       });
   });
 });
--- src/app/shared/services/class.service.ts
+++ src/app/shared/services/class.service.ts
@@ -3,23 +3,18 @@ import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs/Observable';
 
 import { AppStore } from '../reducers/app.store';
-import { CURRENT_CLASS, CLASSES, SET_CURRENT_CLASS } from '../reducers';
-import { Class } from '../models';
+import { CURRENT_CLASS, SET_CURRENT_CLASS } from '../reducers';
 
 @Injectable()
 export class ClassService {
   constructor(private store: Store<AppStore>) {
   }
 
-  getCurrentClass(): Observable<Class> {
+  getCurrentClass(): Observable<string> {
     return this.store.select(CURRENT_CLASS);
   }
 
-  setCurrentClass(currentClass: Class): void {
+  setCurrentClass(currentClass: string): void {
     this.store.dispatch({type: SET_CURRENT_CLASS, payload: currentClass});
-  }
-
-  getAllClasses(): Observable<Array<Class>> {
-    return this.store.select(CLASSES);
   }
 }
