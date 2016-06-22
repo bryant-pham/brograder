854344cd65e54d92d56ee58130fbd54374f46736
bryant <bryantphamm@gmail.com>
2016-06-22 01:49:11 -0500

    Refactor class selector component to get and set current class from selection

--- src/app/student-list/class-selector.component.ts
+++ src/app/student-list/class-selector.component.ts
@@ -1,27 +1,35 @@
 import { Component, Output, EventEmitter } from '@angular/core';
 
-import { TeacherService } from '../shared/services';
+import { TeacherService, ClassService } from '../shared/services';
 import { DropdownMenuComponent } from '../shared/dropdown-menu/dropdown-menu.component';
 
 @Component({
   selector: 'bro-class-selector',
   template: require('./class-selector.html'),
-  providers: [ TeacherService ],
+  providers: [ TeacherService, ClassService ],
   directives: [ DropdownMenuComponent ]
 })
 export class ClassSelectorComponent implements OnInit {
   classes: Array<string>;
+  currentClass: string;
   @Output() selected = new EventEmitter();
 
-  constructor(private teacherService: TeacherService) {
+  constructor(private teacherService: TeacherService,
+              private classService: ClassService) {
   }
 
   ngOnInit() {
     this.teacherService.getTeacher()
       .subscribe(teacher => this.classes = teacher.classes);
+    this.classService.getCurrentClass()
+      .subscribe(currentClass => {
+        this.currentClass = currentClass;
+        this.selected.emit({value: this.currentClass});
+      });
   }
 
   handleClassSelect(selectionEvent): void {
+    this.classService.setCurrentClass(selectionEvent.value);
     this.selected.emit(selectionEvent);
   }
 }
