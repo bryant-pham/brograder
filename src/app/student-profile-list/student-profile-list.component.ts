8f6a2604fccd9a17730806930aa04ea8a88ac57a
bryant <bryantphamm@gmail.com>
2016-06-25 02:48:17 -0500

    Implement student profile list component

--- src/app/student-profile-list/student-profile-list.component.ts
+++ src/app/student-profile-list/student-profile-list.component.ts
@@ -1,11 +1,39 @@
 import { Component } from '@angular/core';
-import { GradeStudentListComponent } from '../grade-student-list/grade-student-list.component';
+
+import { StudentClassSearchPipe } from '../shared/pipes/student-class-filter.pipe';
+import { ClassSelectorComponent } from '../grade-student-list/class-selector.component';
+import { TeacherService, StudentService } from '../shared/services';
+import { Student } from '../shared/models/student.model';
 
 @Component({
   selector: 'student-profile-list',
   template: require('./student-profile-list.html'),
-  directives: [ GradeStudentListComponent ]
+  styles: [ require('../shared/styles/shared.css') ],
+  providers: [ StudentService, TeacherService ],
+  directives: [ ClassSelectorComponent ],
+  pipes: [ StudentClassSearchPipe ]
 })
 export class StudentProfileListComponent {
-  nextRoute = 'Home';
+  students: Array<Student>;
+  classes: Array<string>;
+  selectedClass: string;
+
+  constructor(private studentService: StudentService,
+              private teacherService: TeacherService) {
+  }
+
+  ngOnInit() {
+    this.studentService.getStudents()
+      .subscribe(students => this.students = students);
+    this.teacherService.getTeacher()
+      .subscribe(teacher => this.classes = teacher.classes);
+  }
+
+  setCurrentStudent(student: Student) {
+    this.studentService.setCurrentStudent(student);
+  }
+
+  handleSelectedClass(event): void {
+    this.selectedClass = event.value;
+  }
 }
