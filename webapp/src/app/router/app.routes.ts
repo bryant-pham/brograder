d383595f1aafad7b1d5a38d52449d733f7a44853
bryant <bryantphamm@gmail.com>
2016-06-26 18:18:10 -0500

    Upgrade to angular rc3 and upgrade to new router

---
+++ webapp/src/app/router/app.routes.ts
@@ -0,0 +1,21 @@
+import { provideRouter, RouterConfig } from '@angular/router';
+
+import { LoginComponent } from "../login/login.component";
+import { StudentProfileListComponent } from "../student-profile-list/student-profile-list.component";
+import { GradeStudentListComponent } from "../grade-student-list/grade-student-list.component";
+import { QuestionViewComponent } from "../question-view/question-view.component";
+import { AssignmentListComponent } from "../assignment-list/assignment-list.component";
+import { HomeComponent } from "../home/home.component";
+
+export const routes: RouterConfig = [
+  { path: '', component: HomeComponent },
+  { path: 'assignments', component: AssignmentListComponent },
+  { path: 'questions', component: QuestionViewComponent },
+  { path: 'gradeStudent', component: GradeStudentListComponent },
+  { path: 'class', component: StudentProfileListComponent },
+  { path: 'login', component: LoginComponent },
+];
+
+export const APP_ROUTER_PROVIDERS = [
+  provideRouter(routes)
+];
