4200d0ac545cc32fb7656c78d3be43705744d690
bryant <bryantphamm@gmail.com>
2016-07-02 05:00:08 -0500

    Added initial authenticate method in http service

--- webapp/src/app/shared/services/authentication.service.spec.ts
+++ webapp/src/app/shared/services/authentication.service.spec.ts
@@ -1,21 +1,40 @@
 import { it, describe, expect, beforeEachProviders, beforeEach, inject } from '@angular/core/testing';
+import { provide } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
 
 import { AuthenticationService } from './authentication.service';
+import { HttpMock } from '../mocks/http.mock';
+import { HttpService } from './http.service';
+
+class GoogleUser {
+  getAuthResponse(): string {
+    return 'token';
+  }
+}
 
 describe('AuthenticationService', () => {
   beforeEachProviders(() => [
-    AuthenticationService
+    AuthenticationService,
+    provide(HttpService, {useClass: HttpMock})
   ]);
 
   let service: AuthenticationService;
+  let http: HttpService;
 
-  beforeEach(inject([AuthenticationService], _service => {
+  beforeEach(inject([AuthenticationService, HttpService], (_service, _http) => {
     service = _service;
+    http = _http;
   }));
 
-  it('should return true if authenticated', () => {
-    let result = service.isAuthenticated();
+  it('set authenticated to true if passes authentication', () => {
+    let responseObs = Observable.create(observer => {
+      observer.next('authenticated');
+      observer.complete();
+    });
+    spyOn(http, 'post').and.returnValue(responseObs);
 
-    expect(result).toBeTruthy();
+    service.authenticate(new GoogleUser());
+
+    expect(service.isAuthenticated()).toBeTruthy();
   });
 });
--- webapp/src/app/shared/services/authentication.service.ts
+++ webapp/src/app/shared/services/authentication.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
 
+import { DataService, HttpService } from './http.service.ts';
+
 @Injectable()
 export class AuthenticationService {
+  private authenticated: boolean;
+
+  constructor(private http: HttpService) {
+    this.authenticated = false;
+  }
+
+  authenticate(googleUser): void {
+    let idToken = googleUser.getAuthResponse().id_token;
+    this.http.post('uriToBeDefined', {idToken: idToken})
+      .subscribe(() => {
+        this.authenticated = true;
+      });
+  }
 
   isAuthenticated(): boolean {
-    return true;
+    return this.authenticated;
   }
 }
