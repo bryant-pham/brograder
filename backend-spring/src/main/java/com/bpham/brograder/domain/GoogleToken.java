bdc5ecbcfed805cca73e6fdf83251ec92b7e1bbe
bryant <bryantphamm@gmail.com>
2016-07-04 06:37:53 -0500

    Fix reflection error on GoogleToken

--- backend-spring/src/main/java/com/bpham/brograder/domain/GoogleToken.java
+++ backend-spring/src/main/java/com/bpham/brograder/domain/GoogleToken.java
@@ -1,9 +1,13 @@
 package com.bpham.brograder.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class GoogleToken {
     private String aud;
