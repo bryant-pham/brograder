06479f6a34e5ab7efbfd695c2b7d5e7099c2a9f8
bryant <bryantphamm@gmail.com>
2016-07-04 06:38:48 -0500

    Added application.properties and TokenAuthService bean

--- backend-spring/src/main/java/com/bpham/brograder/Application.java
+++ backend-spring/src/main/java/com/bpham/brograder/Application.java
@@ -1,5 +1,8 @@
 package com.bpham.brograder;
 
+import com.bpham.brograder.domain.service.TokenAuthService;
+import com.bpham.brograder.service.GoogleTokenAuthService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -7,6 +10,9 @@ import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class Application {
+    @Value("${googleOAuth.clientId}")
+    private String oauthClientId;
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
@@ -14,5 +20,10 @@ public class Application {
     @Bean
     public RestTemplate getRestTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public TokenAuthService tokenAuthService() {
+        return new GoogleTokenAuthService(oauthClientId);
     }
 }
