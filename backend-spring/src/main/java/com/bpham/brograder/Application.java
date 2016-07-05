f7049f5718b1f2a4089c91dfa274f519dd3d5133
bryant <bryantphamm@gmail.com>
2016-07-05 00:54:21 -0500

    (spring) Add global cors support

--- backend-spring/src/main/java/com/bpham/brograder/Application.java
+++ backend-spring/src/main/java/com/bpham/brograder/Application.java
@@ -7,6 +7,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class Application {
@@ -25,5 +28,15 @@ public class Application {
     @Bean
     public TokenAuthService tokenAuthService() {
         return new GoogleTokenAuthService(oauthClientId);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
     }
 }
