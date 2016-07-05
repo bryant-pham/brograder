2f23ecca193d4e4234a30b2b7c94bc1977db93dc
bryant <bryantphamm@gmail.com>
2016-07-05 00:54:03 -0500

    (spring) Remove json mapping from token dto

--- backend-spring/src/main/java/com/bpham/brograder/domain/dto/TokenDto.java
+++ backend-spring/src/main/java/com/bpham/brograder/domain/dto/TokenDto.java
@@ -1,10 +1,8 @@
 package com.bpham.brograder.domain.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 
 @Getter
 public class TokenDto {
-    @JsonProperty("id_token")
     private String idToken;
 }
